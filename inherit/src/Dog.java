79b543384df12b8eb7e9c7cc87da1a64d3234c24
shreyaskambla <kshreyas20@gmail.com>
2019-02-04 19:49:22 -0500

    1stcommit

--- inherit/src/Dog.java
+++ inherit/src/Dog.java
@@ -13,13 +13,13 @@ public class Dog extends PetAnimal {
 
         PetAnimal pet = new PetAnimal();
         pet.walk();
-
+        System.out.println("My pet is named"+pet.getName()+" - ID:"+pet.getId());
 
         Dog dog = new Dog();
         dog.bark();
         dog.walk();
         dog.setName("Fido");
-        System.out.println(dog.getName());
+        System.out.println("My dog is named" + dog.getName()+" - ID:" + dog.getId());
     }
 
 }
--- inherit/src/PetAnimal.java
+++ inherit/src/PetAnimal.java
@@ -5,15 +5,29 @@ public class PetAnimal {
     private int age = 1;
     private String name = "";
 
+    private int id;
+    private static int idCounter = 0;
+
+    static {
+        idCounter = 1000;
+        System.out.println("ID counter is "+idCounter);
+    }
+
     /*
      * Constructor
      */
     public PetAnimal(){
         name = "unknown" ;
+        id = idCounter++;
     }
 
     public PetAnimal(String name){
         setName(name);
+        id = idCounter++;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public int getAge() {
@@ -24,6 +38,7 @@ public class PetAnimal {
         if (age < 0) age = 0; //Guard against negative value
         this.age = age;
     }
+
 
     public String getName() {
         return name;
